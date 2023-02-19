ce3300ada87010226240a5d768d0d4be0119e73f
Emilllo <Emily.Ostashevsky@gmail.com>
2023-02-19 14:37:17 +0200

    Update main.py

--- main.py
+++ main.py
@@ -11,21 +11,45 @@ def are_matching(left, right):
 
 def find_mismatch(text):
     opening_brackets_stack = []
+    # closing_brackets_stack = []
     for i, next in enumerate(text):
         if next in "([{":
             # Process opening bracket, write your code here
+            opening_brackets_stack.append(Bracket(next, i + 1))
             pass
-
+                
         if next in ")]}":
             # Process closing bracket, write your code here
+            # closing_brackets_stack.append(Bracket(next, i + 1)
+            if len(opening_brackets_stack) == 0:
+                return i + 1
+            elif not are_matching(opening_brackets_stack[-1].char, next):
+                return i + 1
+
+            opening_brackets_stack.pop()
             pass
+
+    if opening_brackets_stack:
+        return i - 1
+    return "Success"
 
 
 def main():
-    text = input()
-    mismatch = find_mismatch(text)
-    # Printing answer, write your code here
+    fi = input("I or F: ")
+    if "I" in fi or "i" in fi:
+        text = input("Brackets: ")
+        mismatch = find_mismatch(text)
+    elif "F" in fi or "f" in fi:
+        test_num = input("Choose test number (0-5): ")
+        with open(f"test/{test_num}", "r") as file:
+            text = file.read()
+            mismatch = find_mismatch(text)
 
+    # Printing answer, write your code here
+    if mismatch == None:
+        print("Success")
+    else:
+        print(mismatch)
 
 if __name__ == "__main__":
     main()
